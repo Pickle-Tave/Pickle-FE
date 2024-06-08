8e89abe3804aac04829866067fbef452c233bd6d
bourgeois <juahsome2795@gmail.com>
2024-06-09 01:35:45 +0900

    feat: 갤러리 접근 페이지 ui 구현

--- pages/filter/Filter.js
+++ pages/filter/Filter.js
@@ -1,23 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const Arrange = () => {
+const Filter = () => {
   return (
-    <View>
-      <Text>Filter</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.step_0}
+        source={require('../../assets/icon/step_0.png')}
+        resizeMode="contain"
+      />
+      <Text style={styles.text}>사진을 선택하세요!</Text>
+      <Image
+        style={styles.tip}
+        source={require('../../assets/icon/tip.png')}
+        resizeMode="contain"
+      />
+      <Image
+        style={styles.gallery}
+        source={require('../../assets/icon/go_gallery.png')}
+        resizeMode="contain"
+      />
+      <Text style={styles.text_tip}>나만의 추억이 담긴 사진을 정리해요</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  step_0: {
+    width: 300,
+    margin: 10,
+    left: 40,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  tip: {
+    width: 40,
+    height: 40,
+    left: 50,
+    top: 430,
+  },
+  gallery: {
+    width: 370,
+    height: 370,
+    top: -15,
+    left: 15,
+  },
+  text_tip: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    top: 28,
+    left: 20,
+  },
+});
 
-export default Arrange;
+export default Filter;
