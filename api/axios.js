957c835f1498337136f27eff21c405139180c42c
bourgeois <juahsome2795@gmail.com>
2024-06-24 17:32:19 +0900

    feat: 로그인 구현

--- api/axios.js
+++ api/axios.js
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
   baseURL: 'http://pickle-alb-478419970.ap-northeast-2.elb.amazonaws.com', // API 서버 주소
 });
+
+// Axios 인터셉터
+
+instance.interceptors.request.use(
+  async config => {
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
--- components/login/KakaoLoginRedirect.js
+++ components/login/KakaoLoginRedirect.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, StyleSheet, ActivityIndicator, Alert } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, ActivityIndicator, Alert} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { loginKakao } from '../../api/loginKakao';
+import {loginKakao} from '../../api/loginKakao';
 
 const KakaoLoginRedirect = ({handleLoginSuccess}) => {
   const navigation = useNavigation();
@@ -15,15 +15,27 @@ const KakaoLoginRedirect = ({handleLoginSuccess}) => {
         try {
           const loginData = await loginKakao(code);
           if (loginData.success) {
-            await AsyncStorage.setItem('accessToken', loginData.data.accessToken);
-            await AsyncStorage.setItem('refreshToken', loginData.data.refreshToken);
-            Alert.alert('로그인 성공', '카카오 로그인이 성공적으로 완료되었습니다.');
+            await AsyncStorage.setItem(
+              'accessToken',
+              loginData.data.accessToken,
+            );
+            await AsyncStorage.setItem(
+              'refreshToken',
+              loginData.data.refreshToken,
+            );
+            Alert.alert(
+              '로그인 성공',
+              '카카오 로그인이 성공적으로 완료되었습니다.',
+            );
             handleLoginSuccess();
           } else {
             throw new Error('백엔드 서버에서 인증에 실패했습니다.');
           }
         } catch (error) {
-          Alert.alert('로그인 실패', error.message || '서버와 통신하는 중 오류가 발생했습니다.');
+          Alert.alert(
+            '로그인 실패',
+            error.message || '서버와 통신하는 중 오류가 발생했습니다.',
+          );
           navigation.navigate('Onboarding_5'); // 로그인 실패 시 이동할 화면
         }
       } else {
