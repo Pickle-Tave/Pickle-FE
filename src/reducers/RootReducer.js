ea24e147faa3b1752cd3f187f5fe91ea54e91c6b
bourgeois <juahsome2795@gmail.com>
2024-07-01 01:24:50 +0900

    feat: 그룹별 해시태그 상태관리

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,16 +1,16 @@
-import { combineReducers } from "redux";
+import {combineReducers} from 'redux';
 import HashTagReducer from './HashTagReducer';
-import AlbumReducer from "./AlbumReducer";
-import AlbumImageReducer from "./AlbumImageReducer";
-import AlbumLikeReducer from "./AlbumLikeReducer";
-
-
+import AlbumReducer from './AlbumReducer';
+import AlbumImageReducer from './AlbumImageReducer';
+import AlbumLikeReducer from './AlbumLikeReducer';
+import ImageHashTagReducer from './ImageHashTagReducer';
 
 const RootReducer = combineReducers({
-    HashTagReducer,
-    AlbumReducer,
-    AlbumImageReducer,
-    AlbumLikeReducer
-})
+  HashTagReducer,
+  AlbumReducer,
+  AlbumImageReducer,
+  AlbumLikeReducer,
+  ImageHashTagReducer,
+});
 
 export default RootReducer;
